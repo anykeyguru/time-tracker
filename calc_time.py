5f31c53bf502414282c8871069d801e3cbd0d09d
anykeyguru <240509865e@gmail.com>
2020-05-22 17:53:43 +0500

    Optimize 01, Add translate

--- calc_time.py
+++ calc_time.py
@@ -9,16 +9,22 @@ class CounterTime:
     def __init__(self, file, startdate, enddate, lang):
         MFT = '%Y.%m.%d'
         self.LANG = lang
-        pattern = '[0-9][0-9][0-9][0-9]\.[0-1][0-2]\.[0-3][0-9]'
-        if re.match(pattern, startdate) and re.match(pattern, enddate):
-            self.startDate = datetime.strptime(startdate, MFT)
-            self.endDAte = datetime.strptime(enddate, MFT)
+        self.startdate = startdate
+        self.enddate = enddate
+        pattern_a = '[0-9][0-9][0-9][0-9]\.[0-1][0-2]\.[0-3][0-9]'
+        pattern_b = '[0-9][0-9][0-9][0-9]\.0[0-9]\.[0-3][0-9]'
+        if re.match(pattern_a, self.startdate) and re.match(pattern_a, self.enddate)\
+                or re.match(pattern_b, self.startdate) and re.match(pattern_b, self.enddate):
+            self.start_date = datetime.strptime(self.startdate, MFT)
+            self.end_date = datetime.strptime(self.enddate, MFT)
         else:
             print(self.wt('calc', 'p05'), end="")     # No true data, repeat enter
-            self.startDate = datetime.strptime('1900.01.01', MFT)
-            self.endDAte = datetime.strptime('2050.01.01', MFT)
-
+            self.start_date = datetime.strptime('1900.01.01', MFT)
+            self.end_date = datetime.strptime('2050.01.01', MFT)
         self.file = file
+
+
+
 
     def wt(self, win, phrase):
         with open('dict.json', 'r', encoding='utf-8') as f:
@@ -29,8 +35,8 @@ class CounterTime:
         return a
 
     def CountSumm(self):
-        print(self.wt('calc', 'p04'), self.startDate.strftime('%Y.%m.%d') + "/"
-              + self.endDAte.strftime('%Y.%m.%d'), end="\n")
+        print(self.wt('calc', 'p04'), self.start_date.strftime('%Y.%m.%d') + "/"
+              + self.end_date.strftime('%Y.%m.%d'), end="\n")
         regIstrFile = self.file
         read_file = open(regIstrFile)
         lines = read_file.readlines()
@@ -40,7 +46,7 @@ class CounterTime:
             if spl_line not in('RU', 'EN'):
                 MFT = '%Y.%m.%d'
                 spl_line = datetime.strptime(line.split()[0], MFT)
-                if self.startDate <= spl_line <= self.endDAte:
+                if self.start_date <= spl_line <= self.end_date:
                     if line.split()[2] == "Out":
                         ftr = [3600, 60, 1]
                         SSUMM = sum([a * b for a, b in zip(ftr, map(int, line.split()[4].split(':')))])
@@ -63,5 +69,6 @@ class CounterTime:
         else:
             print(self.wt('calc', 'p06'), str(timedelta(seconds=valueTime)) + self.wt('calc', 'p07'), end="\n")
 
-    #   test = counterTime('reglist.txt', '2020.02.04', '2020.02.5')
-    #   test.countSumm
+
+# test = CounterTime('reglist.txt', "2020.05.21", "2020.05.22", 'EN')
+# test.CountSumm()
